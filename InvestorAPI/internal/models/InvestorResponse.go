5d39477ff0e6c17ccb8a50dce5a58e19a25523b5
Hari Iyer <hari.iyer.work@gmail.com>
2024-08-20 08:10:51 +0530

    fixed weird bug with Beginning Balance

--- InvestorAPI/internal/models/InvestorResponse.go
+++ InvestorAPI/internal/models/InvestorResponse.go
@@ -3,11 +3,13 @@ package models
 import (
 	"encoding/json"
 	"time"
+
+	"github.com/markphelps/optional"
 )
 
 type InvestorResponse struct {
 	InterestRate     float64
-	BeginningBalance float64
+	BeginningBalance optional.Float64 `validate:"CustomValidationFloat"`
 	EndingBalance    float64
 	YearsHeld        int
 	TaxRate          float64
